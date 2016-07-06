34b478600f63c7379b1aad87f98b00e1b1f4369a
Kelby Ludwig <kelby.ludwig@praetorian.com>
2016-07-06 17:09:11 -0500

    Making and logging duplicate log entries per request

--- otter.py
+++ otter.py
@@ -80,15 +80,31 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
     #
     
     def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
-    
-        if not messageIsRequest:
+   
+        # Only process responses that came from the proxy. This will
+        # ignore request/responses made by Otter itself.
+        # TODO(kkl): Only process in-scope requests.
+        if not messageIsRequest and toolFlag == self._callbacks.TOOL_PROXY:
             # create a new log entry with the message details
-            self._lock.acquire()
             row = self._log.size()
+
+            # analyze and store information about the original request/response
+            responseBytes = messageInfo.getResponse()
+            requestBytes = messageInfo.getRequest()
             request = self._helpers.analyzeRequest(messageInfo)
-            response_bytes = messageInfo.getResponse()
-            response = self._helpers.analyzeResponse(response_bytes)
-            self._log.add(LogEntry(self._callbacks.saveBuffersToTempFiles(messageInfo), request.getUrl(), response.getStatusCode(), len(response_bytes)))
+            response = self._helpers.analyzeResponse(responseBytes)
+
+            # make a modified request to test for authorization issues
+            # TODO(kkl): Modify the request bytes.
+            modReqResp = self._callbacks.makeHttpRequest(messageInfo.getHttpService(), requestBytes)
+            modRequestBytes = modReqResp.getRequest()
+            modResponseBytes = modReqResp.getResponse()
+            modResponse = self._helpers.analyzeResponse(modResponseBytes)
+
+            entry = LogEntry(self._callbacks.saveBuffersToTempFiles(messageInfo), request.getUrl(), response.getStatusCode(), len(responseBytes), modResponse.getStatusCode(), len(modResponseBytes))
+
+            self._lock.acquire()
+            self._log.add(entry)
             self.fireTableRowsInserted(row, row)
             self._lock.release()
         return
@@ -104,7 +120,7 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
             return 0
 
     def getColumnCount(self):
-        return 3
+        return 5
 
     def getColumnName(self, columnIndex):
         if columnIndex == 0:
@@ -113,6 +129,10 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
             return "Orig. Status"
         if columnIndex == 2:
             return "Orig. Length"
+        if columnIndex == 3:
+            return "Mod. Status"
+        if columnIndex == 4:
+            return "Mod. Length"
         return ""
 
     def getValueAt(self, rowIndex, columnIndex):
@@ -123,6 +143,10 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
             return str(logEntry._origStatus)
         if columnIndex == 2:
             return str(logEntry._origLength)
+        if columnIndex == 3:
+            return str(logEntry._modStatus)
+        if columnIndex == 4:
+            return str(logEntry._modLength)
         return ""
 
     #
@@ -167,9 +191,11 @@ class Table(JTable):
 
 class LogEntry:
 
-    def __init__(self, requestResponse, url, origStatus, origLength):
+    def __init__(self, requestResponse, url, origStatus, origLength, modStatus, modLength):
         self._requestResponse = requestResponse
         self._url = url
         self._origStatus = origStatus
         self._origLength = origLength
+        self._modStatus = modStatus
+        self._modLength = modLength
         return
